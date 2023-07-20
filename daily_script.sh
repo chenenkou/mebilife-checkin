dff7ac9e0a598b4422df2120b4dd6c7e808f3f0f
chenenkou <chenenkou@163.com>
2023-07-20 18:28:17 +0800

    Update sh

--- daily_script.sh
+++ daily_script.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+if [ -f .env ]; then
+  source .env
+fi
+
+cookie="${COOKIE}"
+key="${PUSH_KEY}"
+
 url="https://high.scay.net/user/checkin"
 
 data="{}" 
 
 header="Content-Type: application/json"
+header="$header""\n""charset: UTF-8"
 header="$header""\n""User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82"
 
-cookie="${COOKIE}"
+response=$(curl -X POST -H "$header" -b "$cookie" -d "$data" "$url")
+echo $response
 
-curl -X POST -H "$header" -b "$cookie" -d "$data" "$url"
+msg_data=$(echo "$response" | grep -Eo '"msg":"[^"]*"')
+text=$(echo "$msg_data" | grep -oE '"msg":"[^"]+"' | sed 's/"msg":"\([^"]*\)"/\1/g')
+text="mebilife-checkin: Msg: $text"
+#text=${text//\"/}
+text="${text// /%20}"
+echo $text
+
+url="https://api2.pushdeer.com/message/push?pushkey=${key}&text=${text}"
+curl -X GET "$url"
