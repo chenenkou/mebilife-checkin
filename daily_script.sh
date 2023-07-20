67c0018ff5b8793118b40a28d3ebd59db2152890
chenenkou <chenenkou@163.com>
2023-07-20 18:32:14 +0800

    update sh

--- daily_script.sh
+++ daily_script.sh
@@ -21,7 +21,6 @@ echo $response
 msg_data=$(echo "$response" | grep -Eo '"msg":"[^"]*"')
 text=$(echo "$msg_data" | grep -oE '"msg":"[^"]+"' | sed 's/"msg":"\([^"]*\)"/\1/g')
 text="mebilife-checkin: Msg: $text"
-#text=${text//\"/}
 text="${text// /%20}"
 echo $text
 
