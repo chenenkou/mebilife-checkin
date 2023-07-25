3e5b31c0cc05f0b7458c57937f16dd31e71b6a15
chenenkou <chenenkou@163.com>
2023-07-25 09:27:25 +0800

    Update sh

--- daily_script.sh
+++ daily_script.sh
@@ -8,14 +8,14 @@ cookie="${COOKIE}"
 key="${PUSH_KEY}"
 
 # 发送签到请求
-# url="https://high.scay.net/user/checkin"
-# data="{}"
-# header="Content-Type: application/json"
-# header="$header""\n""charset: UTF-8"
-# header="$header""\n""User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82"
-# response=$(curl -X POST -H "$header" -b "$cookie" -d "$data" "$url")
-# echo $response
-response='{"msg":"\u4f60\u83b7\u5f97\u4e86 113 MB\u6d41\u91cf","ret":1}'
+url="https://high.scay.net/user/checkin"
+data="{}"
+header="Content-Type: application/json"
+header="$header""\n""charset: UTF-8"
+header="$header""\n""User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82"
+response=$(curl -X POST -H "$header" -b "$cookie" -d "$data" "$url")
+echo $response
+# response='{"msg":"\u4f60\u83b7\u5f97\u4e86 113 MB\u6d41\u91cf","ret":1}'
 # 去除所有空格和换行符
 if echo "$response" | grep -q '[[:space:]]'; then
   # 字符串中包含空格或换行符，进行处理
